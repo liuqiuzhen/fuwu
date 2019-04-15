59882b2b81173a87b9e03491b358eaed8f3a336e
yikaiye <yikaiye@yikaiyes-MacBook-Pro.local>
2019-04-15 11:53:22 +0800

    webpack 搭建

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <p>Hello React!</p>
+                <p>Hello React!</p>
+                <p>Hello React!</p>
+                <p>Hello React!</p>
+                <p>默认导出文件更改</p>
+                <p>默认导出文件更改</p>
+                <p>可以了，webpack搭建react项目成功</p>
+            </div>
+    );
+    }
+}
